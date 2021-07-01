f74ae59c5a773685b60dc9174211c2d8f81225ea
Yerlan Nauryzbayev <nauryzbayev.yerlan@gmail.com>
2021-07-01 15:44:49 +0600

    blacked

--- image_loader.py
+++ image_loader.py
@@ -8,35 +8,35 @@ import typing
 import requests
 
 
-TARGET_URL = 'http://127.0.0.1:8000/users/1/upload-avatar/'
-TARGET_FIELD = 'avatar'
-EXTENSIONS = ['jpg', 'jpeg', 'png', 'tiff', 'bmp', 'gif']
+TARGET_URL = "http://127.0.0.1:8000/users/1/upload-avatar/"
+TARGET_FIELD = "avatar"
+EXTENSIONS = ["jpg", "jpeg", "png", "tiff", "bmp", "gif"]
 
 
 def upload_file(filepath, url, url_field):
-    response = requests.post(url, files={
-        url_field: open(filepath, 'rb')
-    })
+    response = requests.post(url, files={url_field: open(filepath, "rb")})
     if response.ok:
-        print(f'{pathlib.Path(filepath).name} uploaded successfully.')
+        print(f"{pathlib.Path(filepath).name} uploaded successfully.")
     else:
-        print(f'{pathlib.Path(filepath).name} was not uploaded.')
+        print(f"{pathlib.Path(filepath).name} was not uploaded.")
 
 
-def get_files_by_extensions(path: str, extensions: typing.List[str]) -> typing.List[str]:
+def get_files_by_extensions(
+    path: str, extensions: typing.List[str]
+) -> typing.List[str]:
     files_path = pathlib.Path(path)
 
     matched_files = []
 
     for extension in extensions:
-        matched_files.extend(files_path.glob(f'*.{extension}'))
+        matched_files.extend(files_path.glob(f"*.{extension}"))
 
     return [str(f.absolute()) for f in matched_files]
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('path', type=str, help='Enter images path.')
+    parser.add_argument("path", type=str, help="Enter images path.")
     args = parser.parse_args()
 
     files = get_files_by_extensions(args.path, EXTENSIONS)
@@ -49,8 +49,8 @@ def main():
                 executor.submit(upload_file, file, TARGET_URL, TARGET_FIELD),
             )
         concurrent.futures.wait(uploads)
-        print('All files uploaded successfully.')
+        print("All files uploaded successfully.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
